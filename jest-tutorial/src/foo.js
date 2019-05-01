78ea9f3d552cfb70ca1c2e5d9274a47d3c8894dc
Harry Robbins <hrobbins@esri.com>
2019-05-01 10:22:49 -0700

    normalize test files

--- jest-tutorial/src/foo.js
+++ jest-tutorial/src/foo.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 export class Foo extends React.Component {
-    sayHi() {
-        alert('hi');
-    }
     render() {
         return(
-            <button onClick={this.sayHi}>Hello World</button>
-            // <button onClick='alert("hi")'>Hello World</button>
+            <button onClick='alert("hi")'>Hello World</button>
         );
     }
 }
