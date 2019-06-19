23059698c30b4c341bacc36145638cd959b45780
Harry Robbins <hrobbins@esri.com>
2019-06-19 12:26:17 -0700

    tests written in typescript are working

--- jest-tutorial/babel.config.js
+++ jest-tutorial/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = ["@babel/preset-react"];
+  const presets = ["@babel/preset-react", "@babel/preset-typescript"];
   const plugins = ["transform-es2015-modules-commonjs"];
 
   return {
